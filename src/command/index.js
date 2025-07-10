d1ae25c1bfe804700e2a3201273da9524e557e6f
zai-server <zailabzayla@gmail.com>
2025-07-10 13:16:27 +0700

    Update and rename sewa.js to index.js

---
+++ src/command/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  sewa: require('./sewa'),
+  redeem: require('./redeem'),
+  user: require('./user'),
+  pengawas: require('./pengawas'),
+  // Tambahkan command lainnya di sini jika ada
+};
